e8b113ab21287619312b37863ccb307ac1c793ca
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2022-03-11 11:05:23 +0100

    add tests

---
+++ Pioggia/PioggiaTest/GameEntityTest.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using NUnit.Framework;
+using OOP21_bullet_ballet_cSharp.Pioggia;
+using OOP21_task_cSharp.Baiocchi;
+using OOP21_task_cSharp.Pioggia;
+
+namespace OOP_21_bullet_ballet_csharp.Pioggia.PioggiaTest
+{
+    public class GameEntityTest
+    {
+        private IPhysicalObject gameObject = new GameEntity(new SpeedVector2DCore(new MutablePosition2DCore(0, 720), 1), new GameEnvironment(0, 720), 1
+            , new Dimension2DCore(2, 2));
+
+        [Test]
+        public void TestMutablePosition2D()
+        {
+            Assert.Equals(gameObject.GetPosition().GetX(), 0.0);
+            Assert.Equals(gameObject.GetPosition().GetY(), 720.0);
+        }
+
+        [Test]
+        public void testMoveUp()
+        {
+            bool isStill = gameObject.MoveUp(800);
+            Assert.True(isStill);
+            isStill = !gameObject.MoveUp(-800);
+            Assert.True(isStill);
+        }
+        
+        [Test]
+        public void testMoveDown()
+        {
+            gameObject.MoveDown(500);
+            Assert.Equals(gameObject.GetPosition().GetX(), 0.0);
+            Assert.Equals(gameObject.GetPosition().GetY(), 1220.0);
+        }
+        
+        [Test]
+        public void testMoveRight()
+        {
+            bool isStill = !gameObject.MoveRight(gameObject.GetGameEnvironment()
+                .getDimension().GetWidth());
+            Assert.True(isStill);
+            bool isMoving = gameObject.MoveRight(gameObject.GetGameEnvironment()
+                .getDimension().GetWidth() - this.gameObject.GetDimension().GetWidth());
+            Assert.True(isMoving);
+        }
+        
+        [Test]
+        public void testMoveLeft()
+        {
+            double testPosition = 180.0;
+            bool isStill = !gameObject.MoveLeft(1);
+            Assert.True(isStill);
+            gameObject.GetPosition().SetPosition(testPosition, gameObject.GetPosition().GetY());
+            bool isMoving = gameObject.MoveLeft(testPosition - this.gameObject.GetDimension().GetWidth());
+            Assert.True(isMoving);
+        }
+        
+    }
+}
