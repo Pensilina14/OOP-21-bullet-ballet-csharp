5b974402e96c53687526babb7f102064e8367490
daze <leon.baiocchi@studio.unibo.it>
2022-03-14 22:55:51 +0100

    Add dimension test

--- Baiocchi/BaiocchiTest/GameEnvironmentTest.cs
+++ Baiocchi/BaiocchiTest/GameEnvironmentTest.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestPlatform.PlatformAbstractions.Interfaces;
 using NUnit.Framework;
+using OOP21_bullet_ballet_cSharp.Pioggia;
+using OOP21_task_cSharp.Pioggia;
 
 
 namespace OOP21_task_cSharp.Baiocchi.BaiocchiTest
@@ -8,11 +10,19 @@ namespace OOP21_task_cSharp.Baiocchi.BaiocchiTest
     public class GameEnvironmentTest
     {
         private readonly IEnvironment _env = new GameEnvironment();
+        private const double DEFAULT_DIM = 20.0;
 
         [Test]
         public void TestGravity()
         {
             Assert.Equals(1.0, _env.getGravity());
+        }
+
+        [Test]
+        public void TestDimension()
+        {
+            IDimension2D expectedDimension = new Dimension2DCore(DEFAULT_DIM, DEFAULT_DIM);
+            Assert.Equals(expectedDimension, _env);
         }
     }
 }
